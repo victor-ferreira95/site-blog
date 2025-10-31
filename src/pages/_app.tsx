3c83789da007844f744d5483b666cd6f66f207ab
victor_ferreira <vitorsantosferreira10@gmail.com>
2025-10-31 00:08:25 -0300

    Diferenças entre _app e _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+// o arquivo _app.tsx é o componente principal da aplicação, executando no servidor e no cliente
+// adiciona estilos globais e configurações da aplicação
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+// o arquivo _document.tsx é o componente principal da aplicação, executando no servidor
+// é responsável por renderizar a página HTML completa, metadados, fontes , etc.
 export default function Document() {
   return (
     <Html lang="en">
