d65de5f69e52feed10ec703ca47367d8eb8a1b67
victor_ferreira <vitorsantosferreira10@gmail.com>
2025-11-09 22:57:51 -0300

    instalação shadcnui

--- src/pages/blog/posts/[slug].tsx
+++
@@ -1,7 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function PostPage() {
-    const router = useRouter();
-    const { slug } = router.query;
-    return <div>PostPage {slug}</div>;
-  }
