b7595ab327adda821cfabb503565e213d509b63a
victor_ferreira <vitorsantosferreira10@gmail.com>
2025-11-12 00:31:56 -0300

    criando o logo

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import { Logo } from '../logo';
 import Link from 'next/link';
 
 export const Footer = () => {
@@ -6,9 +6,7 @@ export const Footer = () => {
     <footer className="border-t border-white/10">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between md:flex-row gap-8 py-8">
-          <Link href="/">
-            <Image src="/logo.svg" alt="Logo site" width={116} height={32} />
-          </Link>
+          <Logo />
 
           <nav className="flex flex-col md:flex-row items-center gap-4 text-sm text-muted-foreground">
             <Link href="/termos-de-uso" className="hover:text-primary">
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,20 +1,14 @@
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import { Button } from '../ui/button';
 import { ActiveLink } from '../active-link';
-import Image from 'next/image';
+import { Logo } from '../logo';
 
 export const Header = () => {
-  const router = useRouter();
-  console.log(router.asPath);
-
   return (
     <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filters]:bg-background/60">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 ls:px-80">
         <div className="flex h-16 items-center justify-between">
-          <Link href="/">
-            <Image src="/logo.svg" alt="Logo site" width={116} height={32} />
-          </Link>
+          <Logo />
 
           <nav className="flex items-center gap-6">
             <ActiveLink href="/">Início</ActiveLink>
