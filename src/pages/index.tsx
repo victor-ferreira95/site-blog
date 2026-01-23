8381029d9e07a6c7078456be91e0d383cf60b3ae
victor_ferreira <vitorsantosferreira10@gmail.com>
2026-01-22 23:50:05 -0300

    creating section hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { HeroSection } from "@/components/hero-section";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className}`}
-    >
-    </div>
+    <>
+    <article className="flex flex-col">
+      <HeroSection />
+    </article>
+  </>
   );
 }
