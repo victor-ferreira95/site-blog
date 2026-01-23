1c47db2117fabca93ae75f4fead06ca143ee8ad6
victor_ferreira <vitorsantosferreira10@gmail.com>
2026-01-23 00:11:35 -0300

    creating section feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { HeroSection } from "@/components/hero-section";
-
+import { FeatureSection } from "@/components/feature-section";
 
 export default function Home() {
   return (
     <>
     <article className="flex flex-col">
       <HeroSection />
-    </article>
+      <FeatureSection />
+    </article>  
   </>
   );
 }
