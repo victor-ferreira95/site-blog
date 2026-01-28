fe40d399ae70a9dd3de20f39fb5aa496112aa537
victor_ferreira <vitorsantosferreira10@gmail.com>
2026-01-27 23:59:23 -0300

    adjusting fonts

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,6 @@
 import { Header } from '../header';
 import { Footer } from '../footer';
-import { Inter } from 'next/font/google';
+import { Inter, PT_Sans_Caption } from 'next/font/google';
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -8,11 +8,18 @@ type LayoutProps = {
 
 const inter = Inter({
   subsets: ['latin'],
+  weight: ['400', '500'],
+  variable: '--font-inter',
+});
+const ptSansCaption = PT_Sans_Caption({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-sans',
 });
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className={`relative flex min-h-screen flex-col bg-gray-700 ${inter.className}`}>
+    <div className={`relative flex min-h-screen flex-col bg-gray-700 ${inter.className} ${ptSansCaption.className}`}>
       <Header />
       <main className="flex-1 flex flex-col mt-10 mb-12">{children}</main>
       <Footer />
