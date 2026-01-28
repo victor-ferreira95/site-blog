4abf1626f76b2cbccc1435fc004b99911c869433
victor_ferreira <vitorsantosferreira10@gmail.com>
2026-01-27 23:06:22 -0300

    creating call to action

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { HeroSection } from "@/components/hero-section";
 import { FeatureSection } from "@/components/feature-section";
 import { SupportSection } from "@/components/support-section";
 import { CustomerStorySection } from "@/components/customer-story-section";
+import { CallToAction } from "@/components/call-to-action";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <FeatureSection />
       <SupportSection />
       <CustomerStorySection />
+      <CallToAction />
     </article>  
   </>
   );
