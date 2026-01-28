557937a0ebb505b887b0d6ddf3b6331064996f20
victor_ferreira <vitorsantosferreira10@gmail.com>
2026-01-28 01:03:14 -0300

    adding images

--- src/components/call-to-action/call-to-action.tsx
+++ src/components/call-to-action/call-to-action.tsx
@@ -4,8 +4,9 @@ import { Button } from '../ui/button';
 
 export const CallToAction = () => {
   return (
-    <section className="py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
-      <div className="container">
+    <section className="relative py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
+      <div className="absolute inset-0 bg-[url('/background-footer.svg')] bg-cover bg-center bg-no-repeat opacity-90" />
+      <div className="container relative">
         <div className="flex flex-col items-center gap-6 text-center">
           <div className="p-4 bg-cyan-300 w-fit rounded-full">
             <Store className="text-cyan-100" />
