d152e3dbac7f9c46f48a797b5c406c9ce95c772d
victor_ferreira <vitorsantosferreira10@gmail.com>
2026-02-05 23:07:25 -0300

    first post created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { LandingPage } from "@/templates/landing-page";
+import { allPosts } from "contentlayer/generated";
 
 export default function Home() {
+  console.debug(allPosts);
   return (
       <LandingPage />
   );
