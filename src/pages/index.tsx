460a434f967e4e23473d2bfdd899fe676fc52c15
victor_ferreira <vitorsantosferreira10@gmail.com>
2026-02-05 23:37:40 -0300

    creating blog page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import { LandingPage } from "@/templates/landing-page";
-import { allPosts } from "contentlayer/generated";
 
 export default function Home() {
-  console.debug(allPosts);
   return (
       <LandingPage />
   );
