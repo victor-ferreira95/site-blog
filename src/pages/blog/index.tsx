85740eec20c2156f8f7f62977339bdf8269b1b5b
victor_ferreira <vitorsantosferreira10@gmail.com>
2026-02-09 23:36:36 -0300

    creating search component

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,3 +1,5 @@
+import { Search } from "@/components/search";
+
 export default function BlogPage() {
   return (
     <div className="flex flex-col py-24 flex-grow h-full">
@@ -14,6 +16,7 @@ export default function BlogPage() {
               Dicas e estratégias para impulsionar seu negócio
             </h1>
           </div>
+          <Search />
         </div>
       </header>
 
